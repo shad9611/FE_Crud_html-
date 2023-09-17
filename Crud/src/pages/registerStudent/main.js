724ced5069231b9f77af9389ad270fd5a45da34e
shad9611 <elohim_shadai@hotmail.com>
2023-09-17 12:48:11 -0700

    modificacion
    modificacion del diseño de la tabla

--- Crud/src/pages/registerStudent/main.js
+++ Crud/src/pages/registerStudent/main.js
@@ -5,13 +5,13 @@ const url = 'http://localhost:8080/api/v1/students';
   .then(data => mostrarData(data))
   .catch(error => console.log(error));
 
- const mostrarData = (data)=>{
+const mostrarData = (data)=>{
   let body = '';
   for (let i = 0; i < data.length;i++){
     body += `<tr><td>${data[i].studentId}</td><td>${data[i].firstName}</td><td>${data[i].lastName}<td>${data[i].email}</td></td></tr>`
   }
   document.getElementById('data').innerHTML = body
- }
+}
   
 
 
